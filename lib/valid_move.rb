a98a0222e1be94e69fe5fd85c205331c30f2dce4
gmat191@gmail.com <gmat191@gmail.com>
2018-06-29 23:51:11 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,18 +3,18 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 index = 0
 
 def valid_move?(board, index)
-   if index > 0 
-     return true
-     index - 1
-   end
-   if index > 8
-     return false
-     elsif position_taken?(board, index) 
-     false 
-   else 
-     true
-   end
+  if index > 0
+    index-1
+  end
+    if index > 8
+      false
+    elsif position_taken?(board, index)
+      false
+    else
+      true
+    end
 end
+
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board, index)
