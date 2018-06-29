b5364391f4c1ec4f6cc1e24cc0d51013722165be
gmat191@gmail.com <gmat191@gmail.com>
2018-06-29 23:43:28 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -19,5 +19,7 @@ end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken(board, index)
-  if board[index] == 0
+  if board[index] == "X" || board[index] == "0"
+    return true 
+    elsif
 end
